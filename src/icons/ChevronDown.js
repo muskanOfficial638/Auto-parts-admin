2688c7a4fa41b40041ce54104b097bb0cfa98b76
guri <guri77566@gmail.com>
2025-12-19 13:59:11 +0530

    fix deploy bug

---
+++ src/icons/ChevronDown.js
@@ -0,0 +1,3 @@
+import * as React from "react";
+const SvgChevronDown = props => <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 20 20" {...props}><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M4.792 7.396 10 12.604l5.208-5.208" /></svg>;
+export default SvgChevronDown;
